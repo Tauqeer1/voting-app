6ebf2a94a731983faf1637d28fad37d45c1b388f
Tauqeer Ahmed <tauqeer.shakir@yahoo.com>
2022-02-10 14:28:10 +0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+interface IVote {
+  title: string;
+  link: string;
+  voteCount: number;
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
+  votingList: Array<IVote> = [];
+  voteObject: IVote;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
 }
